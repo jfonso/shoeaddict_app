a8b584dea095d21d494cd1a4fcc792ac805ab9f7
Javier <javier.alfonso101@alu.ulpgc.es>
2016-04-28 17:18:45 +0000

    Added thumbnails

--- Gemfile
+++ Gemfile
@@ -62,4 +62,5 @@ end
 gem 'puma'
 
 gem 'carrierwave'
+gem 'mini_magick'
 
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,9 +32,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [200, 200]
-  # end
+  version :thumb do
+    process :resize_to_fit => [80, 80]
+  end
 
   # version :normal do
   #   process :resize_to_fit => [350, 350]
